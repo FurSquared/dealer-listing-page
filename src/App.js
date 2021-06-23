fb092857ea2e161d923b84cd13a0bb3fb0b74c81
Draggor <draggor@gmail.com>
2021-06-22 22:22:56 -0500

    Another name change

--- src/App.js
+++ src/App.js
@@ -123,7 +123,7 @@ const App = () => {
       {/* Footer */}
       <footer className={classes.footer}>
         <Typography variant="h6" align="center" gutterBottom>
-          AC Online 2021 Dealers
+          AnthrocOnline™ 2021 Dealers
         </Typography>
         <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
           Created by Draggor
