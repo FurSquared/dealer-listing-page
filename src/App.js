d29977468bf5fbde0c8c79ce6c3471a6fb093374
Draggor <draggor@gmail.com>
2021-06-23 21:29:19 -0500

    Anthrocon Online rename because AnthrocOnline sucks, sorry

--- src/App.js
+++ src/App.js
@@ -123,7 +123,7 @@ const App = () => {
         <Toolbar>
           <CameraIcon className={classes.icon} />
           <Typography variant="h6" color="inherit" noWrap>
-            AnthrocOnline™ 2021 Dealers
+            Anthroco Online 2021 Dealers
           </Typography>
         </Toolbar>
       </AppBar>
@@ -148,7 +148,7 @@ const App = () => {
       {/* Footer */}
       <footer className={classes.footer}>
         <Typography variant="h6" align="center" gutterBottom>
-          AnthrocOnline™ 2021 Dealers
+          Anthroco Online 2021 Dealers
         </Typography>
         <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
           Created by Draggor
